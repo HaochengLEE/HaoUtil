583f41da520f85d79206474f73fbcd9b2f1c40af
lhc <1296760748@qq.com>
2021-08-24 09:55:30 +0800

    JDK 版本降级1.8

--- src/main/java/com/haoutil/chart/calendar/Calendar.java
+++ src/main/java/com/haoutil/chart/calendar/Calendar.java
@@ -44,33 +44,4 @@ public class Calendar {
         return dateMap;
     }
 
-    /**
-     * 按给定范围生成日期，按 Java 9 的新特性做了修改
-     * @param startTime
-     * @param endTime
-     * @param t  value 默认值:可接受 Number 及其子类
-     * @param <T>
-     * @return
-     * @throws ParseException
-     */
-    public static <T extends Number> Map<LocalDate,T> getCalendar9(LocalDate startTime, LocalDate endTime,T t) throws ParseException {
-        //检查起止日期
-        if (startTime==null||endTime==null){
-            throw new IllegalArgumentException("getCalendar 输入日期不合法，有空值");
-        }
-        if (startTime.isAfter(endTime)){
-            throw new IllegalArgumentException("getCalendar 输入日期不合法，截止日期小于起始日期");
-        }
-
-        Map<LocalDate,T> dateMap = new TreeMap<>();
-
-        //迭代日期，间隔日期限制迭代次数
-        Stream.iterate(startTime, d->d.plusDays(1))
-                .takeWhile(d->d.isAfter(endTime))
-                .forEach(
-                        f->dateMap.put(f,t)
-                );
-
-        return dateMap;
-    }
 }
